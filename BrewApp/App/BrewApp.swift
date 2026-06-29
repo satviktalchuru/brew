47b6915222e6d150fb8828055d4b5833555f678d
satviktalchuru <satvik.talchuru@gmail.com>
2026-06-29 16:06:58 -0700

    fix: defer Brew app store wiring

--- BrewApp/App/BrewApp.swift
+++ BrewApp/App/BrewApp.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct BrewApp: App {
-    @State private var store = AppStore.seeded()
-
     var body: some Scene {
         WindowGroup {
             RootView()
-                .environment(store)
         }
     }
 }
